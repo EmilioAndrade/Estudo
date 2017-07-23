c367ad898d9f814178e4b17a2007082aa526fade
Emilio Andrade <Emilio Andrade>
2017-07-23 17:06:07 -0300

    Add project files.

---
+++ Estudos Variados/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudos_Variados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
