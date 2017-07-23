d38c73d42185ae2ba568f073bba5dc2a6d34f7b3
Emilio Moreira de Andrade <Duilio_Andrade@hotmail.com>
2017-07-23 17:18:07 -0300

    Just a test for the first commit.

--- Estudos Variados/Program.cs
+++ Estudos Variados/Program.cs
@@ -10,6 +10,8 @@ namespace Estudos_Variados
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
         }
     }
 }
